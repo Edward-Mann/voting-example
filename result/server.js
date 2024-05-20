fc0dc2416a1b59cb4857f9a78235aa6e8f6fec00
Edward-Mann <Edwardidrism@gmail.com>
2024-05-20 14:30:28 +0100

    fixing styling issues

--- result/server.js
+++ result/server.js
@@ -1,46 +1,46 @@
-const express = require('express');
-const async = require('async');
-const pg = require('pg');
-const { Pool } = require('pg');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
-const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const express = require('express')
+const async = require('async')
+const pg = require('pg')
+const { Pool } = require('pg')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 
-io.set('transports', ['polling']);
+io.set('transports', ['polling'])
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000
 
 io.sockets.on('connection', (socket) => {
-  socket.emit('message', { text: 'Welcome!' });
+  socket.emit('message', { text: 'Welcome!' })
 
   socket.on('subscribe', (data) => {
-    socket.join(data.channel);
+    socket.join(data.channel)
   });
 });
 
 const pool = new pg.Pool({
   connectionString: 'postgres://postgres:postgres@db/postgres'
-});
+})
 
 async.retry(
   { times: 1000, interval: 1000 },
   (callback) => {
     pool.connect((err, client, done) => {
       if (err) {
-        console.error('Waiting for db');
+        console.error('Waiting for db')
       }
       callback(err, client);
-    });
+    })
   },
   (err, client) => {
     if (err) {
-      return console.error('Giving up');
+      return console.error('Giving up')
     }
-    console.log('Connected to db');
+    console.log('Connected to db')
     getVotes(client);
   }
 );
@@ -48,32 +48,32 @@ async.retry(
 function getVotes(client) {
   client.query('SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote', [], (err, result) => {
     if (err) {
-      console.error('Error performing query: ' + err);
+      console.error('Error performing query: ' + err)
     } else {
-      const votes = collectVotesFromResult(result);
-      io.sockets.emit('scores', JSON.stringify(votes));
+      const votes = collectVotesFromResult(result)
+      io.sockets.emit('scores', JSON.stringify(votes))
     }
 
     setTimeout(() => {
-      getVotes(client);
+      getVotes(client)
     }, 1000);
   });
 }
 
 function collectVotesFromResult(result) {
-  const votes = { a: 0, b: 0 };
+  const votes = { a: 0, b: 0 }
 
   result.rows.forEach((row) => {
-    votes[row.vote] = parseInt(row.count);
-  });
+    votes[row.vote] = parseInt(row.count)
+  })
 
-  return votes;
+  return votes
 }
 
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('X-HTTP-Method-Override'))
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
